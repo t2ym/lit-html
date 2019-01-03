c795d81e0ddb6b06a13653261d3c1b561eee9205
Tetsuya Mori <t2y3141592@gmail.com>
2019-01-03 09:37:50 +0900

    1.0.0-rc.1-i18n.6.2 with PoC I18N for demo/clock/ version 6.2

--- demo/clock/clock.js
+++ demo/clock/clock.js
@@ -13,12 +13,33 @@
  */
 
 
-import {html, render, svg} from '../../lit-html.js';
+import {render, svg} from '../../lit-html.js';
+import {repeat} from '../../directives/repeat.js';
+import {html, i18n, bind} from './i18n.js';
+import {getMessage, binding as messageBinding} from './message.js';
+
+const i18nAttrRepoContainer = document.createElement('template');
+i18nAttrRepoContainer.innerHTML = `<i18n-attr-repo>
+  <template id="custom">
+    <div i18n-target-attr="$">
+  </template>
+</i18n-attr-repo>`;
+document.head.appendChild(i18nAttrRepoContainer.content);
 
 /** 
  * Adapted from the Ractive.js clock example: http://www.ractivejs.org/examples/clock/
  */
-export class LitClock extends HTMLElement {
+export class LitClock extends i18n(HTMLElement) {
+
+  static get importMeta() {
+    return import.meta;
+  }
+
+  static get observedAttributes() {
+    let attributes = new Set(super.observedAttributes);
+    [/* list of additional observedAttributes */].forEach(attr => attributes.add(attr));
+    return [...attributes];
+  }
 
   get date() { return this._date; }
   set date(v) { this._date = v; this.invalidate(); }
@@ -26,13 +47,22 @@ export class LitClock extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({mode: 'open'});
+    this.date = new Date();
     setInterval(() => {
       this.date = new Date();
     }, 1000);
+    let _langUpdatedBindThis = this._langUpdated.bind(this);
+    this.addEventListener('lang-updated', _langUpdatedBindThis); // invalidate on this 'lang-updated'
+    messageBinding.element.addEventListener('lang-updated', _langUpdatedBindThis); // invalidate on getMessage()'s 'lang-updated'
+  }
+
+  _langUpdated(event) {
+    // TODO: It should be more efficient to skip successive 'lang-updated' events from different bindings in a short period and invalidate on the last one.
+    this.invalidate();
   }
 
   render() {
-    return html`
+    return html`${bind(this, 'lit-clock')}
       <style>
         :host {
           display: block;
@@ -81,6 +111,20 @@ export class LitClock extends HTMLElement {
           stroke-width: 3;
         }
       </style>
+      <div id='target'
+        @click=${(event) => {
+          let div = event.composedPath().filter(el => el.id === 'target')[0];
+          alert('div.outerHTML = ' + div.outerHTML +
+            ' div.property = ' + div.property +
+            ' div.getAttribute("attr") = ' + div.getAttribute('attr') +
+            ' div.getAttribute("i18n-target-attr") = ' + div.getAttribute('i18n-target-attr'))
+        }}
+        .property=${'property value'}
+        attr=${'attr value'}
+        ?enabled-boolean-attr=${true}
+        ?disabled-boolean-attr=${false}
+        i18n-target-attr="I18N target attribute value">Time: ${this.date.getHours()}:${this.date.getMinutes()}</div>
+      <div>${getMessage()}</div>
       <div class='square'> <!-- so the SVG keeps its aspect ratio -->
         
         <svg viewBox='0 0 100 100'>
@@ -89,9 +133,9 @@ export class LitClock extends HTMLElement {
               all co-ords are relative to the center -->
           <g transform='translate(50,50)'>
             <circle class='clock-face' r='48'/>
-            ${minuteTicks}
-            ${hourTicks}
-    
+            <g>${minuteTicks}</g><!-- g tag to avoid i18n-format conversion -->
+            <g>${hourTicks}</g><!-- g tag to avoid i18n-format conversion -->
+
             <!-- hour hand -->
             <line class='hour' y1='2' y2='-20'
               transform='rotate(${ 30 * this.date.getHours() + this.date.getMinutes() / 2 })'/>
@@ -118,6 +162,20 @@ export class LitClock extends HTMLElement {
         this.needsRender = false;
         render(this.render(), this.shadowRoot);
       });
+    }
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    const handleOnlyBySelf = [];
+    if (!handleOnlyBySelf.indexOf(name) >= 0) {
+      if (typeof super.attributeChangedCallback === 'function') {
+        super.attributeChangedCallback(name, oldValue, newValue);
+      }
+    }
+    switch (name) {
+    //case 'target-attribute': break;
+    default:
+      break;
     }
   }
 }
@@ -150,3 +208,98 @@ const hourTicks = (() => {
   }
   return lines;
 })();
+
+class WorldClock extends LitClock {
+
+  static get importMeta() {
+    return import.meta;
+  }
+
+  get date() { this.__date.setTime(this._date.getTime() + this._date.getTimezoneOffset() * 60 * 1000 + this.timezone * 60 * 1000); return this.__date; }
+  set date(v) { this._date = v; this.invalidate(); }
+
+  constructor() {
+    super();
+    this.__date = new Date();
+    this.timezone = -this._date.getTimezoneOffset();
+  }
+
+  render() {
+    return html`${bind(this, 'world-clock')}
+      <style>
+        :host {
+          display: block;
+          width: 100%;
+          max-width: 350px;
+          padding: 2px;
+        }
+      </style>
+      <div>
+        Timezone: GMT${(this.timezone < 0 ? '' : '+') + (this.timezone / 60)}
+        <button @click=${() => this.timezone -= 60}>-1h</button>
+        <button @click=${() => this.timezone += 60}>+1h</button>
+      </div>
+      ${super.render()}
+    `;
+  }
+
+}
+customElements.define('world-clock', WorldClock);
+
+class WorldClockContainer extends i18n(HTMLElement) {
+
+  static get importMeta() {
+    return import.meta;
+  }
+
+  constructor() {
+    super();
+    this.attachShadow({mode: 'open'});
+    this.timezones = [ 0, -new Date().getTimezoneOffset() ];
+    let _langUpdatedBindThis = this._langUpdated.bind(this);
+    this.addEventListener('lang-updated', _langUpdatedBindThis); // invalidate on this 'lang-updated'
+  }
+
+  _langUpdated(event) {
+    if (this.lang) {
+      this.invalidate();
+    }
+  }
+
+  connectedCallback() {
+    this.connected = true;
+    this.invalidate();
+  }
+
+  render() {
+    return html`${bind(this, 'world-clock-container')}
+      <style>
+        :host {
+          display: block;
+          width: 100%;
+        }
+        world-clock {
+          display: flow;
+          max-width: 300px;
+        }
+      </style>
+      <div>World Clocks</div>
+      ${repeat(this.timezones,
+               (item) => item,
+               (item, index) => 
+                 /* no I18N for this template itself */html`<world-clock .timezone=${item}></world-clock>`)}
+    `;
+  }
+
+  invalidate() {
+    if (!this.needsRender && this.connected) {
+      this.needsRender = true;
+      Promise.resolve().then(() => {
+        this.needsRender = false;
+        render(this.render(), this.shadowRoot);
+      });
+    }
+  }
+
+}
+customElements.define('world-clock-container', WorldClockContainer);
